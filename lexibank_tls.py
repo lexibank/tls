5c2d2b25b5e2b3cfd3cbeeaabfdb4f779f5f1127
Tiago Tresoldi <tresoldi@shh.mpg.de>
2019-10-30 16:31:00 +0100

    Small fixes

--- lexibank_tls.py
+++ lexibank_tls.py
@@ -7,28 +7,9 @@ from pylexibank import progressbar
 from pylexibank.dataset import Dataset as BaseDataset
 
 
-SWAHILI_GLOSS = re.compile(r"(=(?P<gloss>[^)]+))")
-
-
-def normalized(d):
-    match = SWAHILI_GLOSS.search(d["GLOSS"])
-    if match and not d["SWAHILI"]:
-        swa = match.group("gloss").strip()
-        if swa in ["mbogo", "tembo", "moja", "munyu", "tisa", "ndege", "fumo"]:
-            d["GLOSS"] = SWAHILI_GLOSS.sub("", d["GLOSS"]).strip()
-            d["SWAHILI"] = swa
-    return d
-
-
-@attr.s
-class TLSConcept(Concept):
-    Swahili_gloss = attr.ib(default=None)
-
-
 class Dataset(BaseDataset):
     dir = Path(__file__).parent
     id = "tls"
-    concept_class = TLSConcept
 
     # TODO: work on the download
     def cmd_download(self, args):
@@ -60,7 +41,7 @@ class Dataset(BaseDataset):
         # Add concepts
         concept_lookup = args.writer.add_concepts(lookup_factory="Name")
 
-        # TODO: add STEM and PREFIX (pay attention to multiple forms)
+        # TODO: add STEM and PREFIX? (pay attention to multiple forms)
         for entry in progressbar(self.raw_dir.read_csv("tls.txt", dicts=True)):
             # Skip over when language is "Note" (internal comments) or
             # "Gweno1" (a copy of "Gweno")
@@ -74,38 +55,3 @@ class Dataset(BaseDataset):
                 Source=["Nurse1975", "Nurse1979", "Nurse1980", "TLS1999"],
             )
 
-        return
-
-        print(words)
-        return
-
-        with self.cldf as ds:
-            ds.add_sources(self.raw.read("sources.bib"))
-            ds.add_languages()
-            ds.add_concepts(id_factory=lambda c: slug(c.label))
-
-            for i, word in pb(enumerate(words), desc="add words"):
-                if word["LGABBR"]:
-                    # don't carry internal notes
-                    if word["LGABBR"] == "Note":
-                        continue
-
-                    for form in split_text(word["REFLEX"], separators=",;/"):
-                        # remove any additional information (notes, plurals, etc.)
-                        form = strip_brackets(
-                            form, brackets={"[": "]", "{": "}", "(": ")"}
-                        )
-
-                        if form and form != "-":
-                            ds.add_lexemes(
-                                Language_ID=slug(word["LGABBR"]),
-                                Parameter_ID=slug(word["GLOSS"]),
-                                Value=word["REFLEX"],
-                                Source=[
-                                    "Nurse1975",
-                                    "Nurse1979",
-                                    "Nurse1980",
-                                    "TLS1999",
-                                ],
-                                Form=form,
-                            )
